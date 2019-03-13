0e407fc457b02c04c25cb3182f3be2c175f19a09
NateBall4 <43415745+NateBall4@users.noreply.github.com>
2019-03-13 18:47:14 -0400

    Update source.cpp

--- source.cpp
+++ source.cpp
@@ -1,48 +1,73 @@
 #pragma warning(disable:4996)
+#include <iostream>
+#include <conio.h>
+#include <iomanip>	
+#include <windows.h>
 #include "puzzle.h"
 
-#define NUM_ROWS		7		// should not be changed for this solution
-#define NUM_COLS		7		// should not be changed for this soultion
-
-#define PIVOT -1				// used to mark the pivot spot (blank area) on the puzzle
-#define PIVOT_SYMBOL	'*'		// used to show the pivot location when drawing the board
-
-// direction codes (part of the slideTile() interface)
-#define SLIDE_UP		8		// pass to slideTile() to trigger UP movement
-#define SLIDE_DOWN		2		// pass to slideTile() to trigger DOWN movement
-#define SLIDE_LEFT		4		// pass to slideTile() to trigger LEFT movement
-#define SLIDE_RIGHT		6		// pass to slideTile() to trigger RIGHT movement
-
 #define UNSET			-1		// used to arbitrarily indicate an undetermined state in a constuct
-
-#define SCRAMBLE		1000000
-
-#define COLOR_DEFAULT	7
-#define COLOR_RED		12
-#define COLOR_GREEN		10
 
 int main() {
 	// Declarations
 	puzzle sliding_puzzle;
+
 	HANDLE currentConsole;
 	currentConsole = GetStdHandle(STD_OUTPUT_HANDLE);
-	int slidingBoard[NUM_ROWS][NUM_COLS];		// the board that holds the sliding tiles
+	int height = 0;
+	int width = 0;
+	int** pointer = NULL;
 	char keyStroke = ' ';						// holds the user's keystrokes as they come in
 	int directionCode = UNSET;					// used to build a direction code to be sent to slideTile()
 
-	sliding_puzzle.InitializeBoard(slidingBoard);				// initializes the board
-	sliding_puzzle.PrintBoard(slidingBoard, currentConsole);					// prints the board
+	//prompts for height
+	cout << "please enter a height: ";
+	cin >> height;
+	cout << endl;
+
+	//if input fails reprompt
+	while (cin.fail() == true) {
+		cin.clear();
+		rewind(stdin);
+		cout << "invalid input" << endl;
+		cout << "please enter a height: ";
+		cin >> height;
+		cout << endl;
+	}
+	//prompts for width
+	cout << "please enter a width: ";
+	cin >> width;
+	cout << endl;
+
+	//if input fails reprompt
+	while (cin.fail() == true) {
+		cin.clear();
+		rewind(stdin);
+		cout << "invalid input" << endl;
+		cout << "please enter a width: ";
+		cin >> width;
+		cout << endl;
+	}
+
+	//initialize pointer for the dynamic size
+	pointer = new(int*[height]);
+	//make it a 2d array
+	for(int i = 0; i < height; i++){
+		pointer[i] = new(int[width]);
+	 }
+
+	sliding_puzzle.InitializeBoard(pointer, height, width);				// initializes the board
+	sliding_puzzle.PrintBoard(pointer, height, width, currentConsole);					// prints the board
 
 	cout << endl;
 	cout << "Press any key to start!" << endl;	//start prompt
 	_getch();
 
-	sliding_puzzle.scrambleBoard(slidingBoard);				// scrambles the board
+	sliding_puzzle.scrambleBoard(pointer, height, width);				// scrambles the board
 
 	system("cls");								 // clears screen
-	sliding_puzzle.PrintBoard(slidingBoard, currentConsole);	//prints new board
+	sliding_puzzle.PrintBoard(pointer, height, width, currentConsole);	//prints new board
 
-	while (sliding_puzzle.isBoardSolved(slidingBoard) == false) { // while board is unsolved continue game
+	while (sliding_puzzle.isBoardSolved(pointer, height, width) == false) { // while board is unsolved continue game
 		cout << endl;
 		cout << "Please use the numpad arrows to move." << endl;
 		cout << "what direction would you like to move?: " << endl;
@@ -58,14 +83,21 @@ int main() {
 		}
 		system("cls");							//clears the screen
 
-		sliding_puzzle.slideTile(slidingBoard, directionCode); // slides tile
-		sliding_puzzle.PrintBoard(slidingBoard, currentConsole); // prints the board
+		sliding_puzzle.slideTile(pointer, height, width, directionCode); // slides tile
+		sliding_puzzle.PrintBoard(pointer, height, width, currentConsole); // prints the board
 	}
 
 	system("cls"); //clears the screen
-	sliding_puzzle.PrintBoard(slidingBoard, currentConsole);	// prints the solved board
+	sliding_puzzle.PrintBoard(pointer, height, width, currentConsole);	// prints the solved board
 	cout << endl;
 	cout << "congrats you win!";				// win message
+
+		//deallocates the memory given to the pointer
+	for (int i = 0; i < height; i++) {
+		delete[] pointer[i];
+	}
+	delete[] pointer;
+
 	_getch();									// Exit
 	return 0;
 }
